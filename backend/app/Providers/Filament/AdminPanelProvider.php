90057b5bc3796651bb861b4d5a6bc2d2d9f7a2df
MacOS <sbahiyahia19@gmail.com>
2026-02-04 19:01:38 +0100

    add vite

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -18,7 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-
+    
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
---
+++ backend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
